5ca64fa38a96806f8ff6a74300d04a0e4e4b6d56
chaitra2711 <chaitra.hiremath2711@gmail.com>
2026-03-28 13:54:45 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Backend running successfully"
+       return "Backend V3 SUCCESS"
 
 # ✅ ADD THIS (IMPORTANT)
 @app.route("/health")
